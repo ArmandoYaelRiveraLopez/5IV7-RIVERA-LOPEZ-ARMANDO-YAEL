a477c34b9fdb6a70070d027b28f19f02a53bb75e
Amperstream <goopyael33@gmail.com>
2020-10-29 15:04:42 -0600

    retcon

--- CesarPlusUltra/JS/Cifrado.js
+++ CesarPlusUltra/JS/Cifrado.js
@@ -4,52 +4,16 @@ var cesar = cesar || (function(){
     //funcion anonima :3
     //callback
 
-    var doStaff = function(txt, desp, action){
+    var doStaff = function(txt, a, b, action){
         var replace = (function(){
             //mi abecedario
             var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                         'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r',
-                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'];
             var l = abc.length;
 
-            //funcion que se encarga de cifrar
-            return function(c){
-                var i = abc.indexOf(c.toLowerCase());
-                //vamos a verificar que no este vacio
-                if(i != -1){
-                    var pos = i;
-                    if(action){
-                        //avanzar
-                        //en el algoritmo cesar se cifra por desplazamiento
-                        pos += desp;
-                        pos -= (pos>=1)?1:0;
-                    }else{
-                        //retrocedo
-                        //descifrar por le mismo desplazamiento
-                        pos -= desp;
-                        pos += (pos < 0)?1:0;
-                    }
-                    return abc[pos];
-                }
-                return c;
-            };
-        })();
+            
 
-        //aqui es donde tenemos que hacer el match
-        var re = (/([a-z])/ig);
-        return String(txt).replace(re, function(match){
-            return replace(match);
-        });
-    };
-    //ahora solo falta saber si quiero cifrar o descifrar
-    return{
-        encode : function(txt, desp){
-            return doStaff(txt, desp, true);
-        },
-
-        decode : function(txt, desp){
-            return doStaff(txt, desp, false);
-        }
     };
 })();
 
